50d29c6271d6bfed162d107fc9050d5480a30950
Caleb Mutch <cmm2209@columbia.edu>
2024-04-02 13:28:33 +0200

    tweaked js

--- chronology.js
+++ chronology.js
@@ -2,8 +2,8 @@
 let sourcesOrig = [
     { author: 'Song Hyon', title: '<a href="http://www.google.com" class="unblue">Guide to the Study of Music</a>', dating: '1493', postquem: 1493, antequem: 1493, },
     { author: 'Franciscus Salinas', title: '<a href="https://cmm2209.github.io/SalinasDemo/index.html"  class="unblue">De Musica</a>', dating: '1577', postquem: 1577, antequem: 1577, },
-  { author: 'Nakane Akira', title: '<a href="http://www.qwantz.com"  class="unblue">Exhibiting the origin of melodies/notes</a>', dating: '1692', postquem: 1692, antequem: 1692, },
-   { author: 'David the Invincible', title: '<a href="http://www.nytimes.com"  class="unblue">Definitions and Divisions of Philosophy</a>', dating: '6th century?', postquem: 450, antequem: 649, },
+    { author: 'Nakane Akira', title: '<a href="http://www.qwantz.com"  class="unblue">Exhibiting the origin of melodies/notes</a>', dating: '1692', postquem: 1692, antequem: 1692, },
+    { author: 'David the Invincible', title: '<a href="http://www.nytimes.com"  class="unblue">Definitions and Divisions of Philosophy</a>', dating: '6th century?', postquem: 450, antequem: 649, },
 ];
 /* Sort sources chronologically */
 let sources = sourcesOrig.sort((firstItem, secondItem) => firstItem.postquem - secondItem.postquem) ;
@@ -46,3 +46,19 @@ function reset () {
         document.getElementById('sources-here').innerHTML = "No sources found"}
     })};
     chronofilter ();
+
+    
+/*
+  Slidemenu
+*/
+(function() {
+	var $body = document.body
+	, $headerbutton = $body.getElementsByClassName('headerbutton')[0];
+
+	if ( typeof $headerbutton !== 'undefined' ) {
+		$headerbutton.addEventListener('click', function() {
+			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+		});
+	}
+
+}).call(this);
