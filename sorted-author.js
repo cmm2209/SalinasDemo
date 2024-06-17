f7f6ff3b0a3de54a07e1c21a3671bfbcbf9e2a9b
Caleb Mutch <cmm2209@columbia.edu>
2024-06-17 11:20:03 -0400

    Enabled chronofilter for all

--- sorted-author.js
+++ sorted-author.js
@@ -36,6 +36,35 @@ for (i = 0; i < sources.length; i++) {
 document.getElementById('sources-here').innerHTML = parsed};
 populate();
   
+/* Enable reset button */
+function reset () {
+  document.getElementById("reset").addEventListener('click', populate);
+  document.getElementById("reset").addEventListener('click', () => { document.getElementById("earliestDate").value = "-28000";         document.getElementById("latestDate").value = "2020";
+   });
+  };
+  reset ();
+  
+  /* Enable chronological filtering */
+  function chronofilter () {
+  document.getElementById("filter").addEventListener('click', () => {
+    let earliestDate;
+      earliestDate = document.getElementById("earliestDate").value;          let latestDate;
+      latestDate = document.getElementById("latestDate").value;
+      let filteredSources = sources.filter(source => source.antequem >= earliestDate && source.postquem <= latestDate)
+    
+      let parsed = "";
+    if (filteredSources.length >=1) {
+      for (i = 0; i < filteredSources.length; i++) {
+         for (i = 0; i < filteredSources.length; i++) {
+    parsed += "<p class='hit'>" + filteredSources[i].author + ", " + filteredSources[i].title + 
+     " (" + filteredSources[i].dating + ")" + 
+      "</p>" + "<br/>" + "\n";};         
+       document.getElementById('sources-here').innerHTML = parsed}
+  } else {
+      document.getElementById('sources-here').innerHTML = "No sources found"}
+  })};
+  chronofilter ();
+
 /*
   Slidemenu
 */
