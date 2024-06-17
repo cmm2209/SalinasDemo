cd3e2d68a12462ea5634ef2dd5212816e79313ad
Caleb Mutch <cmm2209@columbia.edu>
2024-06-17 10:38:43 -0400

    Added further details to sources

--- sorted-author.js
+++ sorted-author.js
@@ -1,18 +1,18 @@
 /* Source List */
 let sourcesOrig = [
-    { author: 'Song Hyon', title: '<a href="http://www.google.com" class="unblue">Guide to the Study of Music</a>', dating: '1493', postquem: 1493, antequem: 1493, },
-    { author: 'Franciscus Salinas', title: '<a href="https://cmm2209.github.io/SalinasDemo/index.html"  class="unblue">De Musica</a>', dating: '1577', postquem: 1577, antequem: 1577, },
-    { author: 'Nakane Akira', title: '<a href="http://www.qwantz.com"  class="unblue">Exhibiting the origin of melodies/notes</a>', dating: '1692', postquem: 1692, antequem: 1692, },
-    { author: 'David the Invincible', title: '<a href="http://www.nytimes.com"  class="unblue">Definitions and Divisions of Philosophy</a>', dating: '6th century?', postquem: 450, antequem: 649, },
-    { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Late Stone Age Rock Painting</a>', dating: '20,000 to 28,000 BCE', postquem: -28000, antequem: -20000, },
-    { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Julajula (panpipes) and concept of "puru iwalasqa"</a>', dating: '1980s-present', postquem: 1980, antequem: 2024, },
-    { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Silver double-pipes from Ur</a>', dating: '2300 BCE', postquem: -2400, antequem: -2200, },
-    { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">The Wheel of St. John Koukouzeles</a>', dating: '1336 CE', postquem: 1336, antequem: 1366, },
-    { author: 'Lowell Mason', title: '<a href="http://www.nytimes.com"  class="unblue">Manual of the Boston Academy of Music</a>', dating: '1834', postquem: 1834, antequem: 1834, },
-    { author: 'Muḥammad Kāmil al-Ḫulaʿī', title: '<a href="http://www.nytimes.com"  class="unblue">The Book on Oriental Music</a>', dating: '1904-1906', postquem: 1904, antequem: 1906, },
-    { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Solfège annotations in musical manuscripts from the Jesuit archives</a>', dating: '1720s–1760s', postquem: 1720, antequem: 1769, },
-    { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Musica Enchiriadis</a>', dating: 'ca. 850-ca. 900', postquem: 830, antequem: 919, },
-    { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Bells of Kings</a>', dating: '200 AD', postquem: 180, antequem: 220, },
+  { author: 'Song Hyon', title: '<a href="http://www.google.com" class="unblue">Guide to the Study of Music</a>', dating: '1493', postquem: 1493, antequem: 1493, region: 'East Asia', language: 'Korean'},
+  { author: 'Franciscus Salinas', title: '<a href="https://cmm2209.github.io/SalinasDemo/index.html"  class="unblue">De Musica</a>', dating: '1577', postquem: 1577, antequem: 1577, region: 'Europe', language: 'Latin'},
+  { author: 'Nakane Akira', title: '<a href="http://www.qwantz.com"  class="unblue">Exhibiting the origin of melodies/notes</a>', dating: '1692', postquem: 1692, antequem: 1692, region: 'East Asia', language: 'Japanese'},
+  { author: 'David the Invincible', title: '<a href="http://www.nytimes.com"  class="unblue">Definitions and Divisions of Philosophy</a>', dating: '6th century?', postquem: 450, antequem: 649, region: 'Asia', language: 'Armenian'},
+  { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Late Stone Age Rock Painting</a>', dating: '20,000 to 28,000 BCE', postquem: -28000, antequem: -20000, region: 'Africa', language: 'n/a'},
+  { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Julajula (panpipes) and concept of "puru iwalasqa"</a>', dating: '1980s-present', postquem: 1980, antequem: 2024, region: 'South America', language: 'Quechua'},
+  { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Silver double-pipes from Ur</a>', dating: '2300 BCE', postquem: -2400, antequem: -2200, region: 'Near East', language: 'n/a'},
+  { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">The Wheel of St. John Koukouzeles</a>', dating: '1336 CE', postquem: 1336, antequem: 1366, region: 'Europe', language: 'Greek'},
+  { author: 'Lowell Mason', title: '<a href="http://www.nytimes.com"  class="unblue">Manual of the Boston Academy of Music</a>', dating: '1834', postquem: 1834, antequem: 1834, region: 'North America', language: 'English'},
+  { author: 'Muḥammad Kāmil al-Ḫulaʿī', title: '<a href="http://www.nytimes.com"  class="unblue">The Book on Oriental Music</a>', dating: '1904-1906', postquem: 1904, antequem: 1906, region: 'Africa', language: 'Arabic'},
+  { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Solfège annotations in musical manuscripts from the Jesuit archives</a>', dating: '1720s–1760s', postquem: 1720, antequem: 1769, region: 'South America', language: 'n/a'},
+  { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Musica Enchiriadis</a>', dating: 'ca. 850-ca. 900', postquem: 830, antequem: 919, region: 'Europe', language: 'Latin'},
+  { author: 'Anonymous', title: '<a href="http://www.nytimes.com"  class="unblue">Bells of Kings</a>', dating: '200 AD', postquem: 180, antequem: 220, region: 'Africa', language: 'n/a'},
 ];
 /* Sort sources by author */
 let sources = sourcesOrig.sort(function (a, b) {
